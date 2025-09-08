aab1f9a5bb06a7b1dc758232a02d32d0f1ad8c5a
Aarchonth <Aaron.Tielkes.08@gmail.com>
2025-09-08 19:25:21 +0200

    init: first JS website project

---
+++ app.js
@@ -0,0 +1,9 @@
+   
+   <script>
+   const quotes = [
+                    "Stay hungry, stay foolish. – Steve Jobs",
+                    "Simplicity is the soul of efficiency. – Austin Freeman",
+                    "Code is like humor. When you have to explain it, it’s bad. – Cory House",
+                    "First, solve the problem. Then, write the code. – John Johnson"
+            ];
+</script>
